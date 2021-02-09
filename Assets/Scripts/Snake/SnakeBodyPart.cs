f427715a276dca0a1d6f3e432ad30ecd0eca732b
MrBombs42 <stemzan@gmail.com>
2021-02-08 22:12:58 -0300

    clean unused code

--- Assets/Scripts/Snake/SnakeBodyPart.cs
+++ Assets/Scripts/Snake/SnakeBodyPart.cs
@@ -4,17 +4,11 @@ namespace SnakeGame_Arvore_Test.Assets.Scripts
 {
     using UnityEngine;
     
-    public class SnakeBodyPart : MonoBehaviour {        
-        private Animation _animation;
+    public class SnakeBodyPart : MonoBehaviour {
         public Vector3 LastPostion {get;set;}
         [SerializeField] private string _respawnAnimationName; 
 
         private Action<Collider, SnakeBodyPart> _onCollisionCallback;
-
-        void Awake()
-        {
-            _animation = GetComponent<Animation>();
-        }
 
         public void SetOnCollisionCallback(Action<Collider, SnakeBodyPart> onCollision){
             _onCollisionCallback = onCollision;
@@ -31,26 +25,6 @@ namespace SnakeGame_Arvore_Test.Assets.Scripts
             if(_onCollisionCallback != null){
                 _onCollisionCallback(other, this);
             }
-        }
-
-        public void PlayerRespawnAnimation(){
-            if(_animation == null){
-                return;
-            }
-
-            _animation.Play(_respawnAnimationName);
-        }
-
-         public void StopRespawnAnimation(){
-             if(_animation == null){
-                return;
-            }
-           
-            _animation.Rewind(_respawnAnimationName);   
-            _animation.Play(_respawnAnimationName); 
-            _animation.Sample();
-            _animation.Stop(_respawnAnimationName);
-           
         }
 
 
